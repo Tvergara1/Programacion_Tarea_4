2001c4a05539cf7e368ef3bb413688464be2c0a1
Tvergara1 <tavergarac@unadvirtual.edu.co>
2026-05-05 18:46:13 -0400

    agregando sobrecarga en calcular_costo

--- servicios/sala.py
+++ servicios/sala.py
@@ -2,9 +2,20 @@ from modelos.servicio import Servicio
 
 class Sala(Servicio):
 
-    def calcular_costo(self, horas):
+    def calcular_costo(self, horas, descuento=0, impuesto=0):
         if horas <= 0:
             raise ValueError("Horas inválidas")
+            
+        costo = 50 * horas
+
+        if descuento > 0:
+        costo -= costo * descuento
+
+    if impuesto > 0:
+        costo += costo * impuesto
+
+    return costo
+        
         return 50 * horas
 
     def descripcion(self):
